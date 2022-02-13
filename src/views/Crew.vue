e97fb9454f35b29098f9f26f42f1cea2b362a9f6
gsterczewski <sterczewski.dev@protonmail.ch>
2022-02-13 20:40:10 +0100

    FST-2-setup-routing-and-views : add Crew view

---
+++ src/views/Crew.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Crew",
+  components: {},
+});
+</script>
+<template>
+  <div class="crew"></div>
+</template>
+<style scoped></style>
