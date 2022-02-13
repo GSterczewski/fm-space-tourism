a74c2cc73ad8354e454162325c07934581207999
gsterczewski <sterczewski.dev@protonmail.ch>
2022-02-13 20:40:42 +0100

    FST-2-setup-routing-and-views : add Technology view

---
+++ src/views/Technology.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Technology",
+  components: {},
+});
+</script>
+<template>
+  <div class="technology"></div>
+</template>
+<style scoped></style>
