7eaaffffcd0f83f29947465bf0b5e0c4495d69cc
gsterczewski <sterczewski.dev@protonmail.ch>
2022-02-13 14:13:56 +0100

    development: closes FST-3-declare-colors

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,11 @@
   <router-view />
 </template>
 
-<style></style>
+<style>
+:root {
+  /* usage: hsl(var(--variable)) */
+  --color-primary: 0, 100%, 100%;
+  --color-accent: 231, 77%, 90%;
+  --color-dark: 230, 35%, 7%;
+}
+</style>
