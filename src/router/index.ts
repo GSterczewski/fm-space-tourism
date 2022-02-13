f7df9412497d63b55434b64a713b2f7f8a9e29c4
gsterczewski <sterczewski.dev@protonmail.ch>
2022-02-13 20:02:25 +0100

    development: add exact-active class on router-links

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,6 +11,7 @@ const routes: Array<RouteRecordRaw> = [
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
+  linkExactActiveClass: "navlink--active",
   routes,
 });
 
