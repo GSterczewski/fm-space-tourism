e6b352d04bbd1b869366193134b35dac605ffcaf
gsterczewski <sterczewski.dev@protonmail.ch>
2022-02-13 17:59:50 +0100

    development: add VText presentational component

---
+++ src/components/VText.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "VText",
+});
+</script>
+<template>
+  <p class="fs-300 color-accent text">
+    <slot />
+  </p>
+</template>
+<style scoped>
+.text {
+  line-height: 1.6;
+}
+</style>
