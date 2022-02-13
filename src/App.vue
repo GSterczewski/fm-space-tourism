ef617ac15dc07d3e76dd758160a366525b5bf67d
gsterczewski <sterczewski.dev@protonmail.ch>
2022-02-13 15:58:40 +0100

    development: closes FST-4-declare-font-variables

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,39 @@
 
 <style>
 :root {
+  /*-------- COLORS------- */
   /* usage: hsl(var(--variable)) */
   --color-primary: 0, 100%, 100%;
   --color-accent: 231, 77%, 90%;
   --color-dark: 230, 35%, 7%;
+
+  /*-------- FONT SIZES------- */
+  /* 150 */
+  --fs-900: 9.375rem;
+  /* 100 */
+  --fs-800: 6.25rem;
+  /* 80 */
+  --fs-700: 5rem;
+  /* 56 */
+  --fs-600: 3.5rem;
+  /* 40 */
+  --fs-550: 2.5rem;
+  /* 32 */
+  --fs-500: 2rem;
+  /* 28 */
+  --fs-400: 1.75rem;
+  /* 20 */
+  --fs-300: 1.25rem;
+  /* 18 */
+  --fs-250: 1.125rem;
+  /* 16 */
+  --fs-200: 1rem;
+  /* 14 */
+  --fs-100: 0.875rem;
+
+  /*-------- FONT FAMILIES------- */
+
+  --ff-sans: "Barlow Condensed", sans-serif;
+  --ff-serif: "Bellefair", serif;
 }
 </style>
