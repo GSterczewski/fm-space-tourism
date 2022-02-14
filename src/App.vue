ffb8a63d659676b1e2dfd555a1d6a582f8721070
gsterczewski <sterczewski.dev@protonmail.ch>
2022-02-14 08:08:18 +0100

    FST-24 development: add layout wrapper

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
-  <router-view />
+  <div class="layout-wrapper">
+    <router-view />
+  </div>
 </template>
 
 <style>
 @import "./style/global.css";
+.layout-wrapper {
+  min-height: 100vh;
+  display: grid;
+  grid-template-rows: auto 1fr;
+}
 </style>
