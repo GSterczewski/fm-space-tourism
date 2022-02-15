e52ce55358ee60750cb8d6d673e9fa33abc55c41
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2022-02-15 08:00:13 +0100

    FST-10 development: add page content

--- src/views/Destination.vue
+++ src/views/Destination.vue
@@ -1,11 +1,39 @@
 <script lang="ts">
 import { defineComponent } from "vue";
+import useContent from "@/composables/useContent";
+import useCarousel from "@/composables/useCarousel";
+import PageTitle from "@/components/PageTitle.vue";
 export default defineComponent({
   name: "Destination",
-  components: {},
+  components: { PageTitle },
+  setup() {
+    const { destinations } = useContent();
+    const { changeItem, selectedItem } = useCarousel(destinations);
+    return {
+      destinations,
+      changeItem,
+      selectedItem,
+    };
+  },
 });
 </script>
 <template>
-  <div class="destination"></div>
+  <main>
+    <page-title pageTitle="pick your destination" pageNumber="01" />
+    <article class="destination">
+      <img :src="require(`@/assets/destination/${selectedItem.image}.webp`)" />
+      <section>
+        <h2 class="heading ff-serif fs-800">{{ selectedItem.name }}</h2>
+        <p class="color-accent">{{ selectedItem.brief }}</p>
+      </section>
+      <hr />
+      <section>
+        <h3 class="heading ls-200">avg. distance</h3>
+        <p>{{ selectedItem.distance }}</p>
+        <h3 class="heading">est. travel time</h3>
+        <p>{{ selectedItem.travelTime }}</p>
+      </section>
+    </article>
+  </main>
 </template>
 <style scoped></style>
