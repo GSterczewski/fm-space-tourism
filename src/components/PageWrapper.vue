4e9cd951ba732018643acf4ac99697b54958d396
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2022-02-16 15:36:37 +0100

    development: add PageWrapper component to encapsulate common page spacings and title styling

---
+++ src/components/PageWrapper.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import PageTitle from "./PageTitle.vue";
+export default defineComponent({
+  name: "PageWrapper",
+  components: { PageTitle },
+  props: {
+    pageTitle: {
+      type: String,
+    },
+    pageNumber: {
+      type: String,
+    },
+  },
+});
+</script>
+<template>
+  <div class="page-wrapper">
+    <page-title
+      :pageTitle="pageTitle"
+      :pageNumber="pageNumber"
+      class="page-wrapper__title"
+    ></page-title>
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+<style scoped>
+.page-wrapper {
+  padding-inline: clamp(1.5rem, 5vw, 10rem);
+  padding-top: clamp(2rem, 8vh, 5rem);
+  padding-bottom: 5rem;
+}
+</style>
