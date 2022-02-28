472af712883fccaa672fdcc5b15119ab51238910
Grzegorz Sterczewski <sterczewski.dev.protonmail.ch>
2022-02-28 12:03:04 +0100

    FST-31 development: add simple page transitions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
-  <router-view />
+  <router-view v-slot="{ Component }">
+    <transition name="page-switch" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
 
 <style lang="scss">
 @import "./style/global.scss";
+.page-switch-enter-active {
+  transition: opacity 0.25s ease-in;
+}
+.page-switch-leave-active {
+  transition: opacity 0.25s ease-out;
+}
+.page-switch-enter-from,
+.page-switch-leave-to {
+  opacity: 0;
+}
 </style>
